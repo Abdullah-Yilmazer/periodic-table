13fe637c86cafba12ef2e1add1ebe8baf76e0bc7
Abdullah-Yilmazer <abdullahyilmazer50@gmail.com>
2024-10-20 19:37:30 +0300

    yeni animasyon ve içerik güncellendi
    Yeni animasyonlar eklendi.
    readme.md dosyası oluşturuldu.
    elementler daha detaylandırıldı.

---
+++ README.md
@@ -0,0 +1,40 @@
+<p align="center">
+  <img src="./screenshot.png" alt="Screenhot"/>
+</p># Periodic Table App
+
+This project is a Periodic Table application built with vanilla JavaScript and jquery. The application provides visual representation of elements from the periodic table and retrieves detailed information of each element from an internal file. This allows easy access and display of element properties.
+
+
+## Overview
+
+The Periodic Table app provides an interactive way to explore and learn about chemical elements. It is developed using React framework, making it easy to maintain and extend.
+
+## Features
+
+- View the periodic table with detailed information about each element.
+- Click on an element to see its properties such as atomic number, symbol, atomic weight, etc.
+- Interactive design for easy navigation and exploration.
+
+## Required Software
+Requirements
+This project needs to be run on a local server due to the file read operations. To run the project, you can use one of the following tools:
+
+- Visual Studio Code Live Server extension
+- XAMPP
+- Laragon
+
+## Screenshots
+
+![Example](./exampla.gif)
+
+## Data Source
+
+The data used in this project is sourced from the `data.json` file. This file contains detailed information about each element in the periodic table.
+
+## Contributing
+
+Contributions are welcome! If you have any ideas for improvements or new features, feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the [MIT License](https://github.com/Abdullah-Yilmazer/periodic-table/blob/master/LICENSE).
--- js.js
+++ js.js
@@ -1,80 +1,116 @@
 $(document).ready(function () {
-
-  $.ajax(url = "./data.json").done(function (response) {
-    $.each(response.elements, function (i, e) {
-      var element = $(".element")[i]
-      $(element).children(".atomnumarasi").html(e.number)
-      $(element).children(".sembol").html(e.symbol)
-      $(element).children(".isim").html(e.name)
-      $(element).children(".atomKutlesi").html(e.atomic_mass.toString().includes("(") ? e.atomic_mass.split("(", 1) : e.atomic_mass)
+  $.ajax((url = "./data.json")).done(function (response) {
+    $.each(response.en.elements, function (i, e) {
+      var element = $(".element")[i];
+      $(element).children(".atomnumarasi").html(e.number);
+      $(element).children(".sembol").html(e.symbol);
+      $(element).children(".isim").html(e.name);
+      $(element)
+        .children(".atomKutlesi")
+        .html(e.atomic_mass.toString().includes("(") ? e.atomic_mass.split("(", 1) : e.atomic_mass);
       $(element).addClass(`${e.category} ${e.phase} ${e.block}`);
-      $(element).css({ "--color": `#${e.cpkHex ? e.cpkHex.toString() : "000"}`, "grid-area": `${e.ypos}/${e.xpos}` })
+      $(element).css({ "--element-color": `#${e.cpkHex ? e.cpkHex.toString() : "888888"}`, "grid-area": `${e.ypos}/${e.xpos}` });
+      $(element).css({ "--element-bg": `#${e.cpkHex ? e.cpkHex.toString() : "888888"}44`, "grid-area": `${e.ypos}/${e.xpos}` });
     });
   });
 
   //Paneli açma
   $(".element").click(function (e) {
-    var element = Number(e.currentTarget.children[0].innerHTML) - 1
-    $.ajax(url = "./data.json").done(function (response) {
-      var data = response.elements[element]
+    var element = Number(e.currentTarget.children[0].innerHTML) - 1;
+    var elY = e.target.getBoundingClientRect().top;
+    var elX = e.target.getBoundingClientRect().left;
+    console.log(elY, elX,e);
+
+    $.ajax((url = "./data.json")).done(function (response) {
+      var atom = response.en.elements[element];
       var Html = `
-      <img src="${data.image.url}" alt="${data.image.attribution}" title="${data.image.attribution}" width="200px">
-      <model-viewer src="${data.bohr_model_3d}" poster="${data.image.bohr_model_image}" shadow-intensity="1" ar ar-scale="fixed" camera-controls
-        touch-action="pan-y" generate-schema></model-viewer>
-      <p>
-        <span>İsim</span>  
-        <span>${data.name}</span>
-      </p>
-      <p>
-        <span>Atom numarası</span>  
-        <span>${data.number}</span>
-      </p>
-      <p>
-        <span>Faz</span>  
-        <span>${data.phase}</span>
-      </p>`
-      if (data.appearance != null) {
-        Html += `
-      <p>
-        <span>Görünüm</span>  
-        <span>${data.appearance}</span>
-      </p>`
-      }
-      Html += `
-      <div class="dropdown">
-        <input type="checkbox" id="dropdownCheckbox" name="dropdownCheckbox">
-        <label for="dropdownCheckbox" data-toggle="dropdown">Kordinatlar</label>
-        <ul>
-            <li>
-              <span>X pozisyonu</span>  
-              <span>${data.xpos}</span>    
-            </li>
-            <li>
-              <span>y pozisyonu</span>  
-              <span>${data.ypos}</span>    
-            </li>
-            <li>
-              <span>Periyot</span>  
-              <span>${data.period}</span>    
-            </li>
-            <li>
-              <span>Grup</span>  
-              <span>${data.group}</span>    
-            </li>
-        </ul>
-      </div> 
-      <div class="dropdown">
-        <input type="checkbox" id="dropdownCheckbox" name="dropdownCheckbox">
-        <label for="dropdownCheckbox" data-toggle="dropdown">Açıklama</label>
-        <ul>
-            <li>${data.summary}</li>
-        </ul>
-      </div> 
-
-      `
-
-
-      $(".panel").html(Html)
+        <div class="panel"
+          style="
+            --element-bg: #${atom.cpkHex ? atom.cpkHex.toString() : 888888}66 ;
+            --element-color: #${atom.cpkHex ? atom.cpkHex.toString() : 888888};
+            --panel-top: ${elY}px;
+            --panel-left: ${elX}px; "
+          >
+          <div class='image-section'>
+            <span class='sembol'>${atom.symbol}</span>
+            <img src="${atom.image.url}" alt="${atom.image.attribution}" title="${atom.image.attribution}" class='element-image' />
+            ${
+              atom.bohr_model_3d !== null
+                ? `            
+            <div className='model-3D'>
+              <model-viewer src=${atom.bohr_model_3d}
+                poster=${atom.image.bohr_model_image}
+                shadow-intensity=".5"
+                auto-rotate
+                auto-rotate-delay="0"
+                camera-orbit="0deg 70deg 1m" />
+            </div>`
+                : ""
+            }
+          </div>
+          <div class="info-section">
+            <div><span>İsim</span><span>${atom.name}</span></div>
+            <div><span>Atom numarası</span><span>${atom.number}</span></div>
+            <div><span>Faz</span><span>${atom.phase}</span></div>
+            ${
+              atom.appearance !== null
+                ? `<div>
+                <span>Görünüm</span>
+                <span>${atom.appearance}</span>
+              </div>`
+                : ""
+            }
+          </div>
+          <div class='dropdowns'>
+            <div class="dropdown">
+              <input type="checkbox" id="dropdownCheckbox1" name="dropdownCheckbox" />
+              <label for="dropdownCheckbox1" data-toggle="dropdown">Kordinatlar</label>
+              <ul>
+                <li><div><span>X pozisyonu</span><span>${atom.xpos}</span></div></li>
+                <li><div><span>y pozisyonu</span><span>${atom.ypos}</span></div></li>
+                <li><div><span>Periyot</span><span>${atom.period}</span></div></li>
+                <li><div><span>Grup</span><span>${atom.group}</span></div></li>
+              </ul>
+            </div>
+            <div class="dropdown">
+              <input type="checkbox" id="dropdownCheckbox3" name="dropdownCheckbox" />
+              <label for="dropdownCheckbox3" data-toggle="dropdown">Fiziksel Özellikler</label>
+              <ul>
+                ${atom.appearance !== null ? `<li><div><span>Görünüm</span><span>${atom.appearance}</span></div></li>` : ""}
+                ${atom.density !== null ? `<li><div><span>Yoğunluk</span><span>${atom.density} g/cm3</span></div></li>` : ""}
+                ${atom.boil !== null ? `<li><div><span>Kaynama Noktası</span><span>${atom.boil} K</span></div></li>` : ""}
+                ${atom.melt !== null ? `<li><div><span>Ergime Noktası</span><span>${atom.melt} K</span></div></li>` :""}
+                ${atom.phase !== null ? `<li><div><span>Faz</span><span>${atom.phase}</span></div></li>` : ""}
+              </ul>
+            </div>
+            <div class="dropdown">
+              <input type="checkbox" id="dropdownCheckbox4" name="dropdownCheckbox" />
+              <label for="dropdownCheckbox4" data-toggle="dropdown">Elektron Yapılandırması</label>
+              <ul>
+                ${atom.category !== null ? `<li><div><span>Kategori</span><span>${atom.category}</span></div></li>` : ""}
+                ${atom.electron_configuration !== null ? `<li><div><span>Elektron Yapılandırması</span><span>${atom.electron_configuration}</span></div></li>` : ""}
+                ${atom.electron_affinity !== null ? `<li><div><span>Elektron Affinitesi</span><span>${atom.electron_affinity}</span></div></li>` : ""}
+                ${atom.electronegativity_pauling !== null ? `<li><div><span>Elektronegativite (Pauling)</span><span>${atom.electronegativity_pauling}</span></div></li>` : ""}
+                ${
+                  atom.ionization_energies !== null && atom.ionization_energies.length !== 0
+                    ? `<li><div><span>İyonlaşma Enerjileri</span><span>${atom.ionization_energies}</span></div></li>`
+                    : ""
+                }
+              </ul>
+            </div>
+            <div class="dropdown">
+              <input type="checkbox" id="dropdownCheckbox2" name="dropdownCheckbox" />
+              <label for="dropdownCheckbox2" data-toggle="dropdown">Açıklama</label>
+              <ul>
+                ${atom.discovered_by !== null ? `<li><div><span>Keşfeden</span><span>${atom.discovered_by}</span></div></li>` : ""}
+                ${atom.named_by !== null ? `<li><div><span>isim veren</span><span>${atom.named_by}</span></div></li>` : ""}
+                ${atom.summary !== null ? `<li><p><span>${atom.summary}</span></p></li>` : ""}
+              </ul>
+            </div>
+          </div>
+        </div>
+      `;
+      $(".panelMain").html(Html);
     });
 
     $(".panelMain").css("display", "block");
